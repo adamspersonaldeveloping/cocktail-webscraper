1824f1853fe9020d5f3c949e2923d88ebfcce60c
adamspersonaldeveloping <adamspersonaldeveloping@gmail.com>
2024-07-25 21:28:00 -0700

    driver path to env, load env globally, snake case

--- iba-cocktails.py
+++ iba-cocktails.py
@@ -9,18 +9,16 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from dotenv import load_dotenv
 
-#Load env variables
-def load_env():
-    load_dotenv()
-    return os.getenv("DB_STRING")
+# Load env variables
+load_dotenv()
 
-#Connect to MongoDb using pymongo
+# Connect to MongoDb using pymongo
 def connect_database(db_string):
     client = pymongo.MongoClient(db_string)
     db = client["cocktails-apd"]
     return db["cocktails-iba"]
 
-#Set up Selenium and driver
+# Set up Selenium and driver
 def setup_selenium(driver_path):
     options = Options()
     options.headless = True
@@ -29,18 +27,18 @@ def setup_selenium(driver_path):
     driver = webdriver.Chrome(options=options, service=service)
     return driver
 
-#Get URLs from CSV and return a list of them
+# Get URLs from CSV and return a list of them
 def handle_url_csv(file_path):
     with open(file_path) as f:
         reader = csv.reader(f)
         url_list = list(reader)[0]
         return url_list
 
-#Get cocktail data from IBA website
+# Get cocktail data from IBA website
 def get_cocktail_data(driver, url):
     driver.get(url)
 
-    cocktailName = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[1]/div/div/h1').get_attribute('textContent')
+    cocktail_name = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[1]/div/div/h1').get_attribute('textContent')
     ingredients = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div/div/div/div/div[2]/div[1]/p[1]').get_attribute('textContent').split('\n')
     method = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[2]').get_attribute('textContent')
     garnish = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[3]').get_attribute('textContent')
@@ -51,7 +49,7 @@ def get_cocktail_data(driver, url):
         return ingredient_dictionary
 
     cocktail = {
-         'cocktailName': cocktailName,
+         'cocktailName': cocktail_name,
          'ingredients': convert_to_dictionary(ingredients),
          'method': method,
          'garnish': garnish,
@@ -64,22 +62,22 @@ def get_cocktail_data(driver, url):
     return cocktail
 
 def main():
-    db_string = load_env()
+    db_string = os.getenv("DB_STRING")
     col = connect_database(db_string)
-    path = "/Users/stein/Desktop/chromedriver"
-    driver = setup_selenium(path)
+    driver_path = os.getenv("DRIVER_PATH")
+    driver = setup_selenium(driver_path)
     url_list = handle_url_csv("cocktailURLs.csv")
     cocktail_list = []
 
-    #Create a list of cocktails by going over each url and scraping the data for the cocktails
+    # Create a list of cocktails by going over each url and scraping the data for the cocktails
     for url in url_list:
         cocktail = get_cocktail_data(driver, url)
-        cocktail_list.push(cocktail)
+        cocktail_list.append(cocktail)
 
-    #Put the new list into the database
+    # Put the new list into the database
     col.insert_many(cocktail_list)
 
-    #Put the cocktails into a csv file for later
+    # Put the cocktails into a csv file for later
     df = pd.DataFrame(cocktail_list)
     df.to_csv("cocktailsFromIBA.csv", index=False)
 
