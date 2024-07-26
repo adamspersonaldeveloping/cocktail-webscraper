a5c22af514be39b1135cde770e0b4cbfeaf369d4
adamspersonaldeveloping <adamspersonaldeveloping@gmail.com>
2024-07-25 18:02:44 -0700

    refactored

--- iba-cocktails.py
+++ iba-cocktails.py
@@ -1,74 +1,89 @@
-from selenium import webdriver
+import os
+import csv
 import pymongo
 import pandas as pd
-import csv
-import os
-from dotenv import load_dotenv
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv
 
-load_dotenv()
-db = os.getenv("DB_STRING")
-options = Options()
-options.headless = True
-options.add_argument("--window-size=1920,1200")
+#Load env variables
+def load_env():
+    load_dotenv()
+    return os.getenv("DB_STRING")
 
-client = pymongo.MongoClient(db)
-db = client["cocktails-apd"]
-col = db["cocktails-iba"]
+#Connect to MongoDb using pymongo
+def connect_database(db_string):
+    client = pymongo.MongoClient(db_string)
+    db = client["cocktails-apd"]
+    return db["cocktails-iba"]
 
-DRIVER_PATH = '/Users/stein/Desktop/chromedriver'
-service = Service(
-                executable_path=DRIVER_PATH, 
-            )
-driver = webdriver.Chrome(options=options, service=service)
+#Set up Selenium and driver
+def setup_selenium(driver_path):
+    options = Options()
+    options.headless = True
+    options.add_argument("--window-size=1920,1200")
+    service = Service(executable_path=driver_path)
+    driver = webdriver.Chrome(options=options, service=service)
+    return driver
 
-with open("cocktailURLs.csv") as f:
-    reader = csv.reader(f)
-    urlList = list(reader)
-cocktailList = []
+#Get URLs from CSV and return a list of them
+def handle_url_csv(file_path):
+    with open(file_path) as f:
+        reader = csv.reader(f)
+        url_list = list(reader)[0]
+        return url_list
 
-for val in urlList[0]:
+#Get cocktail data from IBA website
+def get_cocktail_data(driver, url):
+    driver.get(url)
 
-     driver.get(val)
-     
-     cocktailName = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[1]/div/div/h1').get_attribute('textContent')
-     
-     ingredients = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div/div/div/div/div[2]/div[1]/p[1]').get_attribute('textContent').split('\n')
-     
-     def Convert(ingredients):
-        ingredientDick= {f'ingredient{i}': ingredients[i] for i in range(0, len(ingredients))}
-        return ingredientDick
+    cocktailName = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[1]/div/div/h1').get_attribute('textContent')
+    ingredients = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div/div/div/div/div[2]/div[1]/p[1]').get_attribute('textContent').split('\n')
+    method = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[2]').get_attribute('textContent')
+    garnish = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[3]').get_attribute('textContent')
+    importance = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div/div/div/div/div[1]/div/div/p/a[2]').get_attribute('textContent')
 
-     method = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[2]').get_attribute('textContent')
+    def convert_to_dictionary(ingredients):
+        ingredient_dictionary = {f'ingredient{i}': ingredients[i] for i in range(0, len(ingredients))}
+        return ingredient_dictionary
 
-     garnish = driver.find_element("xpath", '//*[@id="main-content"]/div/div/div/div/div[2]/div[1]/p[3]').get_attribute('textContent')
-     
-     importance = driver.find_element("xpath", '/html/body/div[1]/div/div/div/div/div/div/div/div[1]/div/div/p/a[2]').get_attribute('textContent')
-     
-     cocktail = {
+    cocktail = {
          'cocktailName': cocktailName,
-         'ingredients': Convert(ingredients),
+         'ingredients': convert_to_dictionary(ingredients),
          'method': method,
          'garnish': garnish,
-         'styleType': importance,
+         'importance': importance,
          'ibaCocktail': 'true',
          'public': 'true',
          'user': 'IBA',
-     }
+    }
 
-     cocktailList.append(cocktail)
+    return cocktail
 
-print(cocktailList)
+def main():
+    db_string = load_env()
+    col = connect_database(db_string)
+    path = "/Users/stein/Desktop/chromedriver"
+    driver = setup_selenium(path)
+    url_list = handle_url_csv("cocktailURLs.csv")
+    cocktail_list = []
 
+    #Create a list of cocktails by going over each url and scraping the data for the cocktails
+    for url in url_list:
+        cocktail = get_cocktail_data(driver, url)
+        cocktail_list.push(cocktail)
 
-x = col.insert_many(cocktailList)
+    #Put the new list into the database
+    col.insert_many(cocktail_list)
 
+    #Put the cocktails into a csv file for later
+    df = pd.DataFrame(cocktail_list)
+    df.to_csv("cocktailsFromIBA.csv", index=False)
 
-df = pd.DataFrame(cocktailList)
-df.to_csv('cocktailsFromIBA.csv')
-print(df.head())
+    driver.quit()
 
-
+if __name__ == "__main__":
+    main()
